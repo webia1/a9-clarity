95672c55352762d4a3d146ea2a4ab4fa77443e92
Erkan Buelbuel <erkan.buelbuel@gmail.com>
2020-01-08 21:03:20 +0100

    ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,6 +17,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     ClarityModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
