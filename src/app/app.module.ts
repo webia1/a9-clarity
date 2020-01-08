0c6dd6fc229f2b1182347d178ac794e499e375dd
Erkan Buelbuel <erkan.buelbuel@gmail.com>
2020-01-08 20:55:41 +0100

    HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +15,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ClarityModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
